48020749d9931fd47ab997f7c507fe0ae9235697
AnnambhotlaK <110944729+AnnambhotlaK@users.noreply.github.com>
2022-12-29 12:53:13 -0500

    distributed code among other files (buggy)

--- MovieRecommendation.py
+++ MovieRecommendation.py
@@ -1,93 +1,27 @@
 # This program will suggest critically acclaimed movies of a specific genre to the user.
+# Data is stored in a HashTable.
 # It also features autocomplete for convenience.
 
-# ------------- LIST OF MOVIES START -------------
+from HashTable.py import *
+from MovieData.py import *
 
-# Action Movies:
-sevenSamurai = ["'Seven Samurai'", "Akira Kurosawa", "1954", "8.6"]
-empireStrikesBack = ["'Star Wars: Episode V - The Empire Strikes Back'", "Irvin Kershner", "1980", "8.7"]
-theMatrix = ["'The Matrix'", "The Wachowskis", "1999", "8.7"]
-gladiator = ["'Gladiator'", "Ridley Scott", "2000", "8.5"]
-theDarkKnight = ["'The Dark Knight'", "Christopher Nolan", "2008", "9.0"]
-actionMovies = [sevenSamurai, empireStrikesBack, theMatrix, gladiator, theDarkKnight]
-
-# Comedy Movies:
-drStrangelove = ["'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb'", "Stanley Kubrick", "1964", "8.4"]
-montyPython = ["'Monty Python and the Holy Grail'", "Terry Gilliam, Terry Jones", "1975", "8.2"]
-groundhogDay = ["'Groundhog Day'", "Harold Ramis", "1993", "8.1"]
-bigLebowski = ["'The Big Lebowski'", "The Coen Brothers", "1998", "8.1"]
-littleMissSunshine = ["'Little Miss Sunshine'", "Jonathan Dayton, Valerie Faris", "2006", "7.8"]
-comedyMovies = [drStrangelove, montyPython, groundhogDay, bigLebowski, littleMissSunshine]
-
-# Drama Movies:
-casablanca = ["'Casablanca'", "Michael Curtiz", "1942", "8.5"]
-theGodfather = ["'The Godfather'", "Francis Ford Coppola", "1972", "9.2"]
-amadeus = ["'Amadeus'", "Milos Forman", "1984", "8.4"]
-shawshankRedemption = ["'The Shawshank Redemption'", "Frank Darabont", "1994", "9.3"]
-inception = ["Inception", "Christopher Nolan", "2010", "8.8"]
-dramaMovies = [casablanca, theGodfather, amadeus, shawshankRedemption, inception]
-
-# Fantasy Movies:
-wizardOfOz = ["'The Wizard of Oz'", "Victor Fleming", "1939", "8.1"]
-beautyAndTheBeast = ["'The Beauty and the Beast'", "Jean Cocteau", "1946", "7.9"]
-princessBride = ["'The Princess Bride'", "Rob Reiner", "1987", "8.0"]
-returnOfTheKing = ["'The Lord of the Rings: The Return of the King'", "Peter Jackson", "2003", "9.0"]
-deathlyHallowsTwo = ["'Harry Potter and the Deathly Hallows: Part 2'", "David Yates", "2011", "8.1"]
-fantasyMovies = [wizardOfOz, beautyAndTheBeast, princessBride, returnOfTheKing, deathlyHallowsTwo]
-
-# Horror Movies:
-psycho = ["'Psycho'", "Alfred Hitchcock", "1960", "8.5"]
-exorcist = ["'The Exorcist'", "William Friedkin", "1973", "8.1"]
-shining = ["'The Shining'", "Stanley Kubrick", "1980", "8.4"]
-silenceOfTheLambs = ["'The Silence of the Lambs'", "Jonathan Demme", "1991", "8.6"]
-getOut = ["'Get Out'", "Jordan Peele", "2017", "7.7"]
-horrorMovies = [psycho, exorcist, shining, silenceOfTheLambs, getOut]
-
-# Mystery Movies:
-citizenKane = ["'Citizen Kane'", "Orson Welles", "1941", "8.3"]
-rearWindow = ["'Rear Window'", "Alfred Hitchcock", "1954", "8.5"]
-chinatown = ["'Chinatown'", "Roman Polanski", "1974", "8.2"]
-se7en = ["'Se7en'", "David Fincher", "1995", "8.6"]
-memento = ["'Memento'", "Christopher Nolan", "2000", "8.4"]
-mysteryMovies = [citizenKane, rearWindow, chinatown, se7en, memento]
-
-# Romance Movies:
-soundOfMusic = ["'The Sound of Music'", "Robert Wise", "1965", "8.1"]
-whenHarryMetSally = ["'When Harry Met Sally...'", "Rob Reiner", "1989", "7.7"]
-titanic = ["'Titanic'", "James Cameron", "1997", "7.9"]
-eternalSunshine = ["'Eternal Sunshine of the Spotless Mind'", "Michel Gondry", "2004", "8.3"]
-slumdogMillionaire = ["'Slumdog Millionaire'", "Danny Boyle", "2008", "8.0"]
-romanceMovies = [soundOfMusic, whenHarryMetSally, eternalSunshine, slumdogMillionaire]
-
-# Thriller Movies:
-nightOfTheHunter = ["'The Night of the Hunter'", "Charles Laughton", "1955", "8.0"]
-deliverance = ["'Deliverance'", "John Boorman", "1972", "7.7"]
-misery = ["'Misery'", "Rob Reiner", "1990", "7.8"]
-changeling = ["'Changeling'", "Clint Eastwood", "2008", "7.8"]
-shutterIsland = ["'Shutter Island'", "Martin Scorsese", "2010", "8.2"]
-thrillerMovies = [nightOfTheHunter, deliverance, misery, changeling, shutterIsland]
-
-# Western Movies:
-highNoon = ["'High Noon'", "Fred Zinnemann", "1952", "8.0"]
-goodBadUgly = ["'The Good, the Bad and the Ugly'", "Sergio Leone", "1966", "8.8"]
-joseyWales = ["'The Outlaw Josey Wales'", "Clint Eastwood", "1976", "7.8"]
-unforgiven = ["'Unforgiven'", "Clint Eastwood", "1992", "8.2"]
-django = ["'Django Unchained'", "Quentin Tarantino", "2012", "8.4"]
-westernMovies = [highNoon, goodBadUgly, joseyWales, unforgiven, django]
-
-# Hashmap used to store data and draw values from.
-# each genre key (string) points to a list of movies value (two-dimensional array).
-movies = { "action" : actionMovies, "comedy" : comedyMovies, "drama" : dramaMovies, 
-"fantasy" : fantasyMovies, "horror" : horrorMovies, "mystery" : mysteryMovies, 
-"romance" : romanceMovies, "thriller" : thrillerMovies, "western" : westernMovies }
-
-# ------------- LIST OF MOVIES END -------------
+# Create the hashmap:
+movies = HashTable(9) # For nine genres
+movies.setVal("action", actionMovies)
+movies.setVal("comedy", comedyMovies)
+movies.setVal("drama", dramaMovies)
+movies.setVal("fantasy", fantasyMovies)
+movies.setVal("horror", horrorMovies)
+movies.setVal("mystery", mysteryMovies)
+movies.setVal("romance", romanceMovies)
+movies.setVal("thriller", thrillerMovies)
+movies.setVal("western", westernMovies)
 
 # Opening greeting/introduction
 def greet():
-    print("* * * * * * * * * * * * * * * * * * * * * * *")
-    print("* * WELCOME TO THE MARVELOUS MOVIE STORE! * *")
-    print("* * * * * * * * * * * * * * * * * * * * * * *")
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * *")
+    print("* * * * WELCOME TO THE MARVELOUS MOVIE STORE! * * * *")
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * *")
     print()
     print("   This establishment features a variety of  ")
     print("   critically-acclaimed films from nine of   ")
@@ -95,7 +29,7 @@ def greet():
     print(" advanced autocomplete to predict the genre of")
     print("    your choice with only a few characters.")
     print()
-    print("- - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * *")
 
 # Search system. Accepts array of options, returns an array of options. 
 # For example, a search for "a" may return ["action", "fantasy", "drama", "romance"]
@@ -105,11 +39,11 @@ def search(options, searchLength):
 
     search = input("Please enter the first character of the genre of your choice: ")
     while len(search) != searchLength:
-        print("Sorry, that is not specific enough!")
+        print("Sorry, we don't understand the input.")
         search = input("Please enter the first character of the genre of your choice: ")
 
-    if options == None:
-        options = movies.keys()
+    #if options == None:
+        #options = movies.keys()
 
     for genre in options:
         if search in genre[0:searchLength]:
@@ -130,13 +64,13 @@ def search(options, searchLength):
             return options
         elif check == 'n':
             print("Sorry! Please try again...")
-            return search(None, 1)
+            return search(movies.keys(), 1)
         else:
             print("Sorry, we didn't understand that. Please search again.")
-            return search(None, 1)
+            return search(movies.keys(), 1)
     else: # Did not work
         print("Sorry, we could not find the genre of your choice.")
-        return search(None, 1)
+        return search(movies.keys(), 1)
 
 # Prints formatted output of movies based on genre
 def output(genre):
@@ -156,11 +90,11 @@ def output(genre):
 # Master function, contains running logic
 def run():
     greet()
-    genre = "".join(search(None, 1)) # Selection as a string
+    genre = "".join(search(movies.keys(), 1)) # Selection as a string
     output(genre)
     repeat = input("Would you like to look for more movies? Enter y/n: ")
     while (repeat == 'y'):
-        genre = "".join(search(None, 1))
+        genre = "".join(search(movies.keys(), 1))
         output(genre)
         repeat = input("Would you like to look for more movies? Enter y/n: ")
         while (repeat != 'y' or repeat != 'n'):
@@ -169,11 +103,5 @@ def run():
     print("Thank you for coming!")
 
 run()
-
-
-
-
-
-
 
 
